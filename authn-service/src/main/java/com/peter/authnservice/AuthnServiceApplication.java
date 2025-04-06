579dcd0769b4f0d4bff975fb1bbf1033e6d5b5af
PeterChen <sanpeter992000@gmail.com>
2025-04-06 20:48:22 +0800

    Initialize Spring Boot project.

---
+++ authn-service/src/main/java/com/peter/authnservice/AuthnServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peter.authnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthnServiceApplication.class, args);
+    }
+
+}
