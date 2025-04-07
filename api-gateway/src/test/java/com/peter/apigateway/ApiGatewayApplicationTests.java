1445db37aa41869ec0eafe03c65c1306b22e81d8
PeterChen <sanpeter992000@gmail.com>
2025-04-07 13:26:49 +0800

    Initialize Spring Boot project.

---
+++ api-gateway/src/test/java/com/peter/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
