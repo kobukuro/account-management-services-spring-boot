3a9cb903c648f6bddd4f7f2673df4acd89917b38
Peter Chen <sanpeter992000@gmail.com>
2025-05-24 21:25:55 +0800

    Add retry and DLT handling for Kafka consumer.

--- notification-service/src/main/java/com/peter/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/peter/notificationservice/NotificationServiceApplication.java
@@ -1,12 +1,20 @@
 package com.peter.notificationservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.peter.notificationservice.domain.event.UserRegistrationEvent;
 import com.peter.notificationservice.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationServiceApplication {
     private final NotificationService notificationService;
 
@@ -17,11 +25,16 @@ public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
-
-
+    @RetryableTopic
     @KafkaListener(topics = "user_registration")
-    public void handleNotification(UserRegistrationEvent userRegistrationEvent) {
+    public void handleNotification(UserRegistrationEvent userRegistrationEvent, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) long offset) throws JsonProcessingException {
+        log.info("Received: {} from {} offset {}", new ObjectMapper().writeValueAsString(userRegistrationEvent), topic, offset);
         notificationService.processNotification(userRegistrationEvent);
+    }
+
+    @DltHandler
+    public void listenDLT(UserRegistrationEvent userRegistrationEvent, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) long offset) throws JsonProcessingException {
+        log.info("DLT Received : {} , from {} , offset {}", new ObjectMapper().writeValueAsString(userRegistrationEvent), topic, offset);
     }
 }
 
