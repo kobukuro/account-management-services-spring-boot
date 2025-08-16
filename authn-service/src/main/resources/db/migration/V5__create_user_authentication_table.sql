b0d7dfca880d030d261190f4887bbd5dec8870c2
Peter Chen <sanpeter992000@gmail.com>
2025-08-16 13:49:06 +0800

    Make password field nullable in user_authentication table.

--- authn-service/src/main/resources/db/migration/V5__create_user_authentication_table.sql
+++ authn-service/src/main/resources/db/migration/V5__create_user_authentication_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE user_authentication
     user_id         UUID                     NOT NULL,
     type            VARCHAR(20)              NOT NULL,
     email           VARCHAR(255)             NOT NULL,
-    password        VARCHAR(255)             NOT NULL,
+    password        VARCHAR(255),
     enabled         BOOLEAN                  NOT NULL DEFAULT FALSE,
     created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
     last_updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
